968e6577ca84eb1465d910cf736d3a0205e5b453
mugihouse <mugi.stnk100122@gmail.com>
2024-02-02 21:51:42 +0900

    Add: 共通レイアウトの適用

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import LinkBookDetails from "@/components/LinkedBookDetails";
 import { getAllReviews } from "@/lib/getter";
+import BooksLayout from "../app/books/[...keyword]/layout";
 
 export const dynamic = "force-dynamic";
 
@@ -7,6 +8,7 @@ export default async function Home() {
   const reviews = await getAllReviews();
   return (
     <>
+      <BooksLayout />
       {reviews.map((b, i) => (
         <LinkBookDetails book={b} index={i + 1} key={b.id} />
       ))}
